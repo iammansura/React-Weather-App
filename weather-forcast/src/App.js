605fb0640afc0d7d4e1021c30137cffc6b3346df
iammansura <mansura.akter091@gmail.com>
2022-07-06 16:59:05 +0200

    added github link

--- weather-forcast/src/App.js
+++ weather-forcast/src/App.js
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="App">
       <h1>Weather-Forcast</h1>
+      <p>
+        This projects was codded by{' '}
+        <a href="https://github.com/iammansura" target="-blank">
+          Mansura
+        </a>{' '}
+        and is{' '}
+        <a href="https://github.com/iammansura/weather-forcast" target="-blank">
+          open-source on GitHub
+        </a>
+      </p>
     </div>
   )
 }
