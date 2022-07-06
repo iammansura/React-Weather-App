3e817ed8203130d4930ac257a99b8112a5d59649
iammansura <mansura.akter091@gmail.com>
2022-07-06 22:06:55 +0200

    import react

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
+import React from 'react'
 import './App.css'
 import Searchengine from './components/SearchEngine'
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <Searchengine city="Dhaka" />
+    <>
+      <div className="App">
+        <div className="container">
+          <Searchengine city="Dhaka" />
 
-        <footer className="text-center">
-          This projects was codded by{' '}
-          <a href="https://github.com/iammansura" target="-blank">
-            Mansura
-          </a>{' '}
-          and is{' '}
-          <a
-            href="https://github.com/iammansura/weather-forcast"
-            target="-blank"
-          >
-            open-source on GitHub
-          </a>
-        </footer>
+          <footer className="text-center">
+            This projects was codded by{' '}
+            <a href="https://github.com/iammansura" target="-blank">
+              Mansura
+            </a>{' '}
+            and is{' '}
+            <a
+              href="https://github.com/iammansura/weather-forcast"
+              target="-blank"
+            >
+              open-source on GitHub
+            </a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
