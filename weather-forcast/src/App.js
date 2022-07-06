81e53e4652529b5d4fc145a65baeb30040ec43be
iammansura <mansura.akter091@gmail.com>
2022-07-06 17:31:17 +0200

    added bootstrap

--- weather-forcast/src/App.js
+++ weather-forcast/src/App.js
@@ -3,17 +3,22 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <h1>Weather-Forcast</h1>
-      <p>
-        This projects was codded by{' '}
-        <a href="https://github.com/iammansura" target="-blank">
-          Mansura
-        </a>{' '}
-        and is{' '}
-        <a href="https://github.com/iammansura/weather-forcast" target="-blank">
-          open-source on GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1>Weather-Forcast</h1>
+        <footer>
+          This projects was codded by{' '}
+          <a href="https://github.com/iammansura" target="-blank">
+            Mansura
+          </a>{' '}
+          and is{' '}
+          <a
+            href="https://github.com/iammansura/weather-forcast"
+            target="-blank"
+          >
+            open-source on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   )
 }
