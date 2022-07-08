4e34efe3a99b601c64b2099d757ebf92a5061bfd
iammansura <mansura.akter091@gmail.com>
2022-07-08 14:28:47 +0200

    added linkedin

--- src/App.js
+++ src/App.js
@@ -13,18 +13,28 @@ function App() {
           />
 
           <footer className="text-center">
-            This projects was coded by{' '}
-            <a href="https://github.com/iammansura" target="-blank">
-              Mansura
-            </a>{' '}
-            and is{' '}
             <a
               href="https://github.com/iammansura/weather-forcast"
               target="-blank"
             >
-              open-sourced on GitHub
+              Open-sourced on GitHub
             </a>{' '}
             and <br />
+            <a
+              href="https://react-weather-forcastapp.netlify.app"
+              target="-blank "
+            >
+              {' '}
+              hosted on Netlify
+            </a>{' '}
+            by
+            <a
+              href="https://www.linkedin.com/in/mansura-akter2022/"
+              target="-blank"
+            >
+              Mansura Akter
+            </a>{' '}
+            and is and <br />
             <a
               href="https://react-weather-forcastapp.netlify.app"
               target="-blank "
