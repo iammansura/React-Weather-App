37376f51225db447de8cbaff997a98973e1d0a2a
iammansura <mansura.akter091@gmail.com>
2022-07-08 12:59:34 +0200

    styles code

--- src/components/DailyForcast.jsx
+++ src/components/DailyForcast.jsx
@@ -18,6 +18,17 @@ export default function DailyForcast(props) {
     setLoaded(true)
   }
 
+  function load() {
+    let apiKey = '5f472b7acba333cd8a035ea85a0d4d4c'
+    let longitude = props.coords.lon
+    let latitude = props.coords.lat
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+
+    axios.get(apiUrl).then(handleResponse)
+
+    return null
+  }
+
   if (loaded) {
     return (
       <div className="Forcast">
@@ -54,13 +65,7 @@ export default function DailyForcast(props) {
       </div>
     )
   } else {
-    let apiKey = '5f472b7acba333cd8a035ea85a0d4d4c'
-    let longitude = props.coords.lon
-    let latitude = props.coords.lat
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-
-    axios.get(apiUrl).then(handleResponse)
-
+    load()
     return null
   }
 }
